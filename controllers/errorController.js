83d3b529b5bd2298476499ce25a0eedbf54ddfeb
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2021-11-17 20:50:51 -0800

    Implement more errors handling for both productions and development

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,61 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsErrorDB = (err) => {
+  const value = err.message.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  console.log(value);
+  const message = `Value: ${value} is a duplicate field. Please try a different name`;
+  return new AppError(message, 400);
+};
+
+const handleValidationsErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input. ${errors}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.log(err.message, err.name);
+    console.log(err.errors);
+    res.status(500).json({
+      status: 'Error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   // console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    // let error = { ...err };
+
+    // if (err.name === 'CastError') err = handleCastErrorDB(err);
+    if (err.code === 11000) err = handleDuplicateFieldsErrorDB(err);
+    if (err.name === 'ValidationError') err = handleValidationsErrorDB(err);
+    sendErrorProd(err, res);
+  }
 };

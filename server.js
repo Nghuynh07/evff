7195fe5fed19a001d1984cf890a5f377711a964a
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2022-03-02 20:18:11 -0800

    test

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ const port = process.env.PORT || 4000;
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+const DB = process.env.DATABASE;
 
 mongoose
   .connect(DB, {

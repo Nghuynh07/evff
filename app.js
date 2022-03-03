28c0b88f0336dfddd4337880d1099f67ff348cde
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2022-03-02 16:24:26 -0800

    added more stuff

--- app.js
+++ app.js
@@ -30,6 +30,10 @@ const publicPath = path.join(__dirname, 'public');
 
 app.use(express.static(publicPath));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../front_end/build'));
+}
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });

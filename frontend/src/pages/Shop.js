d4f529448370c49f633759e42434c33054382ad6
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2022-03-07 23:17:21 -0800

    test

--- frontend/src/pages/Shop.js
+++ frontend/src/pages/Shop.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import Product from '../products/Product';
 import ProductsContainer from '../layout/ProductsContainer';
 import { ProductContext } from '../store/product-context';
@@ -6,12 +6,13 @@ import { ProductContext } from '../store/product-context';
 const Shop = () => {
   const pContext = useContext(ProductContext);
   const { loading, products } = pContext;
-  // console.log(products);
+
+  console.log(products.length);
 
   return (
     <ProductsContainer>
       {loading &&
-        products.length > 0 &&
+        products !== 0 &&
         products.map((product) => (
           <Product key={product._id} product={product} />
         ))}

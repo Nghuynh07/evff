32daf9ca73c968e0fd48de42d568b4678d624754
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2022-03-07 21:16:38 -0800

    test

--- frontend/src/pages/Shop.js
+++ frontend/src/pages/Shop.js
@@ -2,27 +2,12 @@ import React, { useContext, useState } from 'react';
 import Product from '../products/Product';
 import ProductsContainer from '../layout/ProductsContainer';
 import { ProductContext } from '../store/product-context';
-import axios from 'axios';
-import { useEffect } from 'react';
 const Shop = () => {
-  // const pContext = useContext(ProductContext);
-  // const products = pContext.products;
-
-  const [products, setProducts] = useState([]);
-
-  const getProducts = async () => {
-    await axios.get('/api/v1/products').then((res) => {
-      setProducts(res.data.data);
-    });
-  };
-
-  useEffect(() => {
-    getProducts();
-  }, []);
+  const pContext = useContext(ProductContext);
 
   return (
     <ProductsContainer>
-      {products.map((product) => (
+      {pContext.products.map((product) => (
         <Product key={product._id} product={product} />
       ))}
     </ProductsContainer>
